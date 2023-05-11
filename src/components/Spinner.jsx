978e1fc6628db192303ef6656ab907683e6293e4
Baruti-1 <barutibaruani1@gmail.com>
2023-05-11 18:09:16 +0300

    Create spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from "../assets/Spinner.svg";
+
+export default function Spinner() {
+  return (
+    <div className="bg-black bg-opacity-50 flex items-center justify-center fixed top-0 bottom-0 left-0 right-0 z-50">
+      <div>
+        <img src={spinner} alt="loading ..." className="h-24" />
+      </div>
+    </div>
+  );
+}
