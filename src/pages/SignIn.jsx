e5d5d5c7670b649e02a383c94e8be97d3d3f4d16
Baruti-1 <barutibaruani1@gmail.com>
2023-05-11 10:37:44 +0300

    Implement sign in with  email and password

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import OAuth from "../components/OAuth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default function SignIn() {
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
@@ -19,6 +22,24 @@ export default function SignIn() {
     }));
   }
 
+  const handleSignIn = async (e) => {
+    try {
+      e.preventDefault();
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredentials.user;
+      if (user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Invalid email or password");
+    }
+  };
+
   return (
     <section>
       <h1 className="text-center text-3xl mt-6 font-bold">Sign In</h1>
@@ -31,7 +52,7 @@ export default function SignIn() {
           />
         </div>
         <div className="mb-6 w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={handleSignIn}>
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="email"
