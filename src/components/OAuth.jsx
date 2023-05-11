66ad559e325ece856dfaf8f9d58965c4e5a40bfd
Baruti-1 <barutibaruani1@gmail.com>
2023-05-11 10:14:25 +0300

    Implement signup by google

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,41 @@
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+
+  const googleSignUp = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // check if user already exist
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Google sign up failed");
+    }
+  };
+
   return (
-    <button className="flex items-center justify-center bg-red-600 text-white px-7 py-3 w-full rounded hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out">
+    <button
+      type="button"
+      onClick={googleSignUp}
+      className="flex items-center justify-center bg-red-600 text-white px-7 py-3 w-full rounded hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
