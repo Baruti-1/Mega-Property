5980cbd520f6ab49014a70459786f76514f65f06
Baruti-1 <barutibaruani1@gmail.com>
2023-05-11 16:27:52 +0300

    Create private route

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
+export function PrivateRoute() {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+}
