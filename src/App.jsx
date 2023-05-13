d8658c7c32de78137dd9978fdac590a35e494263
Baruti-1 <barutibaruani1@gmail.com>
2023-05-13 21:23:35 +0300

    Fix errors in category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
-import ForgotPassword from "./pages/ForgotPassword";
-import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import CreateListing from "./pages/CreateListing";
-import Listing from "./pages/Listing";
 import EditListing from "./pages/EditListing";
-
+import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 function App() {
   return (
     <>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />,
+          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/forgot-password" element={<ForgotPassword />} />,
-          <Route path="/offers" element={<Offers />} />,
-          <Route path="/profile" element={<Profile />} />,
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
-          <Route path="/sign-in" element={<SignIn />} />,
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/create-listing" element={<PrivateRoute />}>
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
           <Route path="edit-listing" element={<PrivateRoute />}>
