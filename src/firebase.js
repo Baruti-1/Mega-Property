26f631b0003cfc5e4bf954892b8f97ae7ef311fe
Baruti-1 <barutibaruani1@gmail.com>
2023-05-13 21:11:55 +0300

    Complete offers page

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "mega-property-3c21b.firebaseapp.com",
-  projectId: "mega-property-3c21b",
-  storageBucket: "mega-property-3c21b.appspot.com",
-  messagingSenderId: "446607962258",
-  appId: "1:446607962258:web:7114a860de16bba055364b",
-  measurementId: "G-4KYPB0D7FH",
+  apiKey: "AIzaSyC2b9DfCwVmWYgC7zLjaHTt3Aj09t4WhNs",
+  authDomain: "mega-property-1d9f1.firebaseapp.com",
+  projectId: "mega-property-1d9f1",
+  storageBucket: "mega-property-1d9f1.appspot.com",
+  messagingSenderId: "343168757226",
+  appId: "1:343168757226:web:25e6f15f681af1d8625693",
+  measurementId: "G-Y8GK20KYJ9",
 };
 
 // Initialize Firebase
